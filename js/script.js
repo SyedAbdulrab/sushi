f2fbdf87169ce1ffdc07bc40fd491b43e865f710
SyedAbdulrab <syedabdulrab1133@gmail.com>
2023-10-01 17:10:56 +0500

    add aos animations

---
+++ js/script.js
@@ -0,0 +1,10 @@
+import Aos from "aos";
+import "aos/dist/aos.css";
+
+// init AOS animation
+Aos.init({
+    duration: 1000,
+    offset: 100,
+});
+
+// rest of the stuff will be done in index.html
